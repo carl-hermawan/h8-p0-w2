dbe582c6784bbc27c77d63a1dfd5733d186b126d
Carl <carlh3h@yahoo.co.id>
2019-09-24 14:05:17 +0700

    fix if-else.js

--- if-else.js
+++ if-else.js
@@ -4,11 +4,11 @@ var template = 'Halo ' + peran + ' ' + nama + ", ";
 
 if (nama === '') {
   console.log("Nama harus diisi!");
-} else if (nama !== '' && peran === ''){
-  console.log("Halo " + nama + ", Pilih peranmu untuk memulai game!");
 } else if (nama !== ''){
   console.log("Selamat datang di Dunia Proxytia, " + nama);
-  if (peran === 'Ksatria'){
+  if (peran === ''){
+    console.log("Halo " + nama + ", Pilih peranmu untuk memulai game!");
+  } else if (peran === 'Ksatria'){
     console.log(template + "kamu dapat menyerang dengan senjatamu!");
   } else if (peran === 'Tabib'){
     console.log(template + "kamu akan membantu temanmu yang terluka.");
