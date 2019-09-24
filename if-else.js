2977d39ab35a096ae903f824cd6204bf76c76558
Carl <carlh3h@yahoo.co.id>
2019-09-24 14:02:00 +0700

    add if-else anchor

--- if-else.js
+++ if-else.js
@@ -3,10 +3,10 @@ var peran = '';
 var template = 'Halo ' + peran + ' ' + nama + ", ";
 
 if (nama === '') {
-	console.log("Nama harus diisi!");
-}	else if (nama !== '' && peran === ''){
-	console.log("Halo " + nama + ", Pilih peranmu untuk memulai game!");
-}	else if (nama !== ''){
+  console.log("Nama harus diisi!");
+} else if (nama !== '' && peran === ''){
+  console.log("Halo " + nama + ", Pilih peranmu untuk memulai game!");
+} else if (nama !== ''){
   console.log("Selamat datang di Dunia Proxytia, " + nama);
   if (peran === 'Ksatria'){
     console.log(template + "kamu dapat menyerang dengan senjatamu!");
